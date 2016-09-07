a77e9a7aa99e61da39f09c475871ea2f0a4f1b59
Andrew Crist <andrew.l.crist@gmail.com>
2016-09-07 17:50:40 -0600

    Incorporated objects into JS.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 $('.save-button').on('click', function(){
   var title = $('.title').val();
   var body = $('.body').val();
+  createIdea(title, body);
   prependIdeaToList(title, body);
   store();
 });
@@ -11,25 +12,26 @@ function getUserInput(){
 }
 
 function store() {
-  var title = $('.title').val();
-  var body = $('.body').val();
   var stringedTitle = JSON.stringify(title);
   var stringedBody = JSON.stringify(body);
-  localStorage.setItem('title', stringedTitle);
-  localStorage.setItem('body', stringedBody);
+  localStorage.setItem('idea', stringedTitle);
+  // localStorage.setItem('body', stringedBody);
 }
 
 function prependIdeaToList(title, body) {
   $('.list-container').prepend('<div class="list-item"><li class="title-style">' + title + '<img src="icons/delete.svg" height="20" width="20"></li><li class="body-style">' + body + '</li><img src="icons/downvote.svg" height="20" width="20"><img src="icons/upvote.svg" height="20" width="20"><p class="quality">quality: </p>');
 }
 
+function Idea(title, body) {
+  this.title = title;
+  this.body = body;
+  this.id = Date.now();
+  this.quality = 'swill';
+}
 
+function createIdea(title, body) {
+  var idea  = new Idea(title, body);
+  console.log(idea);
+  debugger;
 
-// function Idea(title, body, id, quality) {
-//   var title =  $('.title').val();
-//   var body = $('.body').val();
-//   this.title = title;
-//   this.body = body;
-//   this.id = Date.now();
-//   this.quality = quality;
-// }
+}
