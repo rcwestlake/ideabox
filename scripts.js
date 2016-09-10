bffadeacf7f9240846f08e0afe947edddc1e705a
Ryan Westlake <ryancwestlake@gmail.com>
2016-09-10 15:43:45 -0600

    removed commented out code

--- scripts.js
+++ scripts.js
@@ -9,7 +9,6 @@ $('.save-button').on('click', function(){
   var idea = new Idea(title, body);
   AllIdeas.addStoreToArray(idea);
   AllIdeas.retrieve();
-  // AllIdeas.render(idea);
   AllIdeas.clearListContainer();
   AllIdeas.renderStorage();
 });
@@ -90,20 +89,12 @@ var AllIdeas = {
     console.log(localStorage.setItem('ideasArray', JSON.stringify(ideasArray)));
   },
 
-  // storeIdea: function() {
-  //   localStorage.setItem('idea', JSON.stringify(idea));
-  // },
-
   render: function(idea) {
     $('.list-container').prepend('<div class="list-item"' + 'id="' + idea.id + '"><li class="title-style"><input value=' + idea.title + '><img src="icons/delete.svg" height="20" width="20"></li><li class="body-style"><input value=' + idea.body + '></li><img class="downvote" src="icons/downvote.svg" height="20" width="20"><img class="upvote" src="icons/upvote.svg" height="20" width="20"><p class="quality">quality: ' + '<span class="quality-value">' + idea.quality + '</span>' + '</p></div>');
   },
 
   renderStorage: function() {
-    //before we use ideasArray, check localStorage and make sure ideasArray matches
     for (var i = 0; i < ideasArray.length; i++) {
-     //iterate through the ideasArray array.
-        //identify the individual titles, body, id, buttons, and quality for each object
-        //prepend to the page in the right order
         var object = ideasArray[i];
         var idea = new Idea(object.title, object.body, object.id, object.quality);
         ideasArray[i] = idea;
